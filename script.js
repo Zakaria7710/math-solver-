0cf10086b1aa5b28cd31d4c99338da6a01c60ea5
Zakaria7710 <zokafm24@gmail.com>
2024-08-31 23:30:32 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function solveProblem() {
+    const problem = document.getElementById("problem").value;
+    
+    // هنا يمكنك إضافة منطق الحل الخاص بك
+    // مثال بسيط: فقط إرجاع المدخلات
+    document.getElementById("solution").innerText = "الحل: " + problem; // استبدل هذا بالحل الحقيقي
+}
